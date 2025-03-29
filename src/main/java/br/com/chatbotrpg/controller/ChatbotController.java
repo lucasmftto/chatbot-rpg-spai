a3c292834635da9ab0a560417357a0813195a753
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2025-03-29 19:02:54 -0300

    Criacao de recurso para incluir novos arquivos

--- src/main/java/br/com/chatbotrpg/controller/ChatbotController.java
+++ src/main/java/br/com/chatbotrpg/controller/ChatbotController.java
@@ -4,8 +4,12 @@ import br.com.chatbotrpg.service.ChatbotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController()
 public class ChatbotController {
@@ -14,8 +18,17 @@ public class ChatbotController {
     private ChatbotService service;
 
     @GetMapping("/chatbot")
-    public ResponseEntity<String> model(@RequestParam(value = "message", defaultValue = "Hello") String message) {
+    public ResponseEntity<String> model(
+            @RequestParam(value = "message", defaultValue = "Hello") String message) {
 
-        return  ResponseEntity.ok(this.service.callChatbot(message));
+        return ResponseEntity.ok(this.service.callChatbot(message));
+    }
+
+    @PostMapping("/load-file")
+    public ResponseEntity<Void> loadFile(
+            @RequestParam("file") MultipartFile file) throws IOException {
+
+        this.service.loadFiles(file);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/br/com/chatbotrpg/service/ChatbotService.java
+++ src/main/java/br/com/chatbotrpg/service/ChatbotService.java
@@ -7,13 +7,20 @@ import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.ai.document.Document;
-import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.reader.markdown.MarkdownDocumentReader;
+import org.springframework.ai.reader.markdown.config.MarkdownDocumentReaderConfig;
+import org.springframework.ai.transformer.KeywordMetadataEnricher;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -26,9 +33,6 @@ public class ChatbotService {
 
     @Autowired
     private OpenAiChatModel openAiChatClient;
-
-    @Autowired
-    private EmbeddingModel embeddingModel;
 
     @Autowired
     private OpenAiChatModel chatModel;
@@ -67,5 +71,25 @@ public class ChatbotService {
         return chatResponse.getResults().stream()
                 .map(generation -> generation.getOutput().getText())
                 .collect(Collectors.joining("/n"));
+    }
+
+    public void loadFiles(MultipartFile file) throws IOException {
+        Resource resource = new InputStreamResource(file.getInputStream());
+        MarkdownDocumentReaderConfig config = MarkdownDocumentReaderConfig.builder()
+                .withAdditionalMetadata("title", file.getOriginalFilename())
+                .withAdditionalMetadata("filename", file.getOriginalFilename())
+                .build();
+
+        MarkdownDocumentReader reader = new MarkdownDocumentReader(resource, config);
+
+        TokenTextSplitter splitter = new TokenTextSplitter(5000,
+                400, 10, 5000, true);
+
+        List<Document> split = splitter.split(reader.get());
+        KeywordMetadataEnricher enricher = new KeywordMetadataEnricher(this.chatModel, 5);
+        List<Document> enriched = enricher.apply(split);
+
+        this.vectorStore.add(enriched);
+
     }
 }
